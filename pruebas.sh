b83a1f7818970e34f5f0e9fe7b41cbfdfdb4cee8
ManuelBlanc <manuel.blanc@estudiante.uam.es>
2017-11-01 16:06:18 +0100

    Refactorizado el script de pruebas, corregido erratas
    + Faltaba un ejecutable en el .gitignore
    + Cambios cosmeticos en src/alfa.l

--- pruebas.sh
+++ pruebas.sh
@@ -3,106 +3,120 @@ set -eu
 
 die() { >&2 echo "$@"; exit 1; }
 
-## Carpeta con binarios
-BDIR="."
-
-## Carpeta con objetos
-ODIR="obj"
-
-## Carpeta con otros recursos
-MDIR="misc"
-
-## Objeto alfalib
-ALFALIB="$ODIR/alfalib.o"
+BDIR="."                  # Carpeta con binarios
+ODIR="obj"                # Carpeta con objetos
+MDIR="misc"               # Carpeta con otros recursos
+ALFALIB="$ODIR/alfalib.o" # Objeto alfalib
 
 ## Colorines
-NC='\033[0m'
-GREEN='\033[0;32m'
-RED='\033[0;31m'
+NC="$(tput sgr0)"
+BOLD="$(tput setaf 7)"
+REVERSE="$(tput smso)"
+RED="$(tput setaf 1)"
+GREEN="$NC$(tput setaf 2)"
 
-# Navegamos al directorio del script
+## Navegamos al directorio del script
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# './pruebas.sh clean' borra lo generado
-if [ $# -eq 1 ] && [ $1 = clean ]; then
+## Rutina auxiliar que limpia todos los ficheros generados
+clean_and_exit() {
+	#make clean
+	rm -f prueba1 prueba2 prueba3
+	rm -f prueba1.o prueba2.o prueba3.o
+	rm -f prueba1.nasm prueba2.nasm prueba3.nasm
+	exit 0
+}
 
-    make clean
-    rm -f prueba1 prueba2 prueba3
-    rm -f prueba1.o prueba2.o prueba3.o
-    rm -f prueba1.nasm prueba2.nasm prueba3.nasm
-    exit 0
+## Imrprime una cabecera con formato
+title()    { echo -e "$REVERSE[$*]$NC"; }
+subtitle() { echo -e    "$BOLD[$*]$NC"; }
+
+## Un diff un poco mas descriptivo
+diff() {
+	echo '> diff' "$@"
+	echo -en "$RED"
+	command diff "$@"
+
+	if [[ $? -eq 0 ]]; then
+		echo -e "${GREEN}OK$NC"
+	else
+		echo -e "${RED}ERR$NC"
+	fi
+}
+
+# './pruebas.sh clean' borra lo generado
+if [[ "$*" == 'clean' ]]; then
+	clean_and_exit
 fi
 
 # Compilamos tests en carpeta BDIR
+compile() {
+	title Compilando pruebas
+	make test all
+	echo
+}
 
-echo [Compilando pruebas]
-make test
-make all
+practica1() {
+	title 'Pruebas practica 1 - generacion'
 
-echo [Pruebas practica 1 - generacion]
-# Pruebas con los datos del enunciado
-echo [Prueba1]
-echo -e -n $RED
+	# Pruebas con los datos del enunciado
+	subtitle Prueba 1.1
 
-$BDIR/main_0_generacion_ejemplo1 prueba1.nasm
-nasm -f elf32 -o $ODIR/prueba1.o prueba1.nasm
-gcc -m32 -o prueba1 $ODIR/prueba1.o $ALFALIB
+	$BDIR/main_0_generacion_ejemplo1 prueba1.nasm
+	nasm -f elf32 -o $ODIR/prueba1.o prueba1.nasm
+	gcc -m32 -o prueba1 $ODIR/prueba1.o $ALFALIB
 
-diff <(./prueba1 <<< $'-9\n') <(echo -n $'-1\n')
-diff <(./prueba1 <<< $'10\n') <(echo -n $'18\n')
-echo -e $GREEN[OK]$NC
+	diff <(./prueba1 <<< $'-9\n') <(echo -n $'-1\n')
+	diff <(./prueba1 <<< $'10\n') <(echo -n $'18\n')
 
-echo [Prueba2]
-echo -e -n $RED
+	subtitle Prueba 1.2
 
-$BDIR/main_0_generacion_ejemplo2 prueba2.nasm
-nasm -f elf32 -o $ODIR/prueba2.o prueba2.nasm
-gcc -m32 -o prueba2 $ODIR/prueba2.o $ALFALIB
+	$BDIR/main_0_generacion_ejemplo2 prueba2.nasm
+	nasm -f elf32 -o $ODIR/prueba2.o prueba2.nasm
+	gcc -m32 -o prueba2 $ODIR/prueba2.o $ALFALIB
 
-diff <(./prueba2 <<< $'0\n') <(echo -n $'true\nfalse\n')
-diff <(./prueba2 <<< $'1\n') <(echo -n $'false\ntrue\n')
-echo -e $GREEN[OK]$NC
+	diff <(./prueba2 <<< $'0\n') <(echo -n $'true\nfalse\n')
+	diff <(./prueba2 <<< $'1\n') <(echo -n $'false\ntrue\n')
 
-echo [Prueba3]
-echo -e -n $RED
+	subtitle Prueba 1.3
 
-$BDIR/main_0_generacion_ejemplo3 prueba3.nasm
-nasm -f elf32 -o $ODIR/prueba3.o prueba3.nasm
-gcc -m32 -o prueba3 $ODIR/prueba3.o $ALFALIB
+	$BDIR/main_0_generacion_ejemplo3 prueba3.nasm
+	nasm -f elf32 -o $ODIR/prueba3.o prueba3.nasm
+	gcc -m32 -o prueba3 $ODIR/prueba3.o $ALFALIB
 
-diff <(./prueba3 <<< $'-3\n3\n0\n') <(echo -n $'true\n3\n0\n3\n')
-diff <(./prueba3 <<< $'10\n-9\n1\n') <(echo -n $'false\n-10\n1\n-9\n')
-echo -e $GREEN[OK]$NC
+	diff <(./prueba3 <<< $'-3\n3\n0\n') <(echo -n $'true\n3\n0\n3\n')
+	diff <(./prueba3 <<< $'10\n-9\n1\n') <(echo -n $'false\n-10\n1\n-9\n')
 
-echo [Pruebas practica 2 - tabla simbolos]
-echo -e -n $RED
+	echo
+}
 
-diff <($BDIR/main_1_tabla $MDIR/p2_entrada_1.txt) <(cat $MDIR/p2_salida_1.txt)
+practica2() {
+	title 'Pruebas practica 2 - tabla simbolos'
 
-echo -e $GREEN[OK]$NC
+	diff <($BDIR/main_1_tabla $MDIR/p2_entrada_1.txt) "$MDIR/p2_salida_1.txt"
 
-echo [Pruebas practica 3 - analizador morfologico]
+	echo
+}
 
-echo [Prueba 1]
-echo -e -n $RED
-diff <($BDIR/pruebaMorfo $MDIR/p3_entrada_1.txt) <(cat $MDIR/p3_salida_1.txt)
-echo -e $GREEN[OK]$NC
+practica3() {
+	title 'Pruebas practica 3 - analizador morfologico'
 
-echo [Prueba 2]
-echo -e -n $RED
+	subtitle Prueba 3.1
+	diff <($BDIR/pruebaMorfo $MDIR/p3_entrada_1.txt) "$MDIR/p3_salida_1.txt"
 
-diff <($BDIR/pruebaMorfo $MDIR/p3_entrada_2.txt 2> /dev/null) <(cat $MDIR/p3_salida_2.txt)
-echo -e $GREEN[OK]$NC
+	subtitle Prueba 3.2
+	diff <($BDIR/pruebaMorfo $MDIR/p3_entrada_2.txt 2> /dev/null) "$MDIR/p3_salida_2.txt"
 
-echo [Prueba 3]
-echo -e -n $RED
-diff <($BDIR/pruebaMorfo $MDIR/p3_entrada_3.txt 2> /dev/null) <(cat $MDIR/p3_salida_3.txt)
-echo -e $GREEN[OK]$NC
+	subtitle Prueba 3.3
+	diff <($BDIR/pruebaMorfo $MDIR/p3_entrada_3.txt 2> /dev/null) "$MDIR/p3_salida_3.txt"
 
-echo [Borrando ficheros generados]
-make clean
-rm -f prueba1 prueba2 prueba3
-rm -f $ODIR/prueba1.o $ODIR/prueba2.o $ODIR/prueba3.o
-rm -f prueba1.nasm prueba2.nasm prueba3.nasm
+	echo
+}
 
-echo -e $GREEN [Pruebas correctas] $NC
+compile
+practica1
+practica2
+practica3
+
+title 'Borrando ficheros generados'
+clean_and_exit
